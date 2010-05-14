abc8396e6660582cda3d2cc2ee3c00b9b04f01f0
mmorton <mmorton@icanhascode.com>
2010-05-13 21:56:06 -0700

    applied fix for simplate sub-template issue.

--- applications/mobile/content/javascript/Simplate.js
+++ applications/mobile/content/javascript/Simplate.js
@@ -82,7 +82,7 @@
             fragments[i] = "__p('" + fragments[i].replace(/'/g, "\\'").replace(/\n/g, "\\n") + "');";
            
         var source = [
-            'var __r = [], $ = __v || {}, __s = Simplate; __p = function() { __r.push.apply(__r, arguments); };',
+            'var __r = [], $ = __v || {}, __s = Simplate, __p = function() { __r.push.apply(__r, arguments); };',
             'with ($) {',
             fragments.join(''),
             '}',
