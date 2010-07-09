f75e2f7c64f3241d7b3e3d648eb28e0ea5a26664
mmorton <mmorton@icanhascode.com>
2010-07-09 14:15:24 -0700

    removed application files as they were placed in their own repository

--- build/mobile-dependencies.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-%JAVA_HOME%\bin\java.exe -Dfile.encoding=UTF-8 -jar "../tools\JSBuilder\JSBuilder2.jar" -v -p "./mobile-dependencies.jsb2" -d "../deploy"
-if %errorlevel% neq 0 exit /b %errorlevel% 
-xcopy ..\deploy\mobile-dependencies\*.* ..\applications\mobile\packages /Y
--- build/mobile-slx.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-%JAVA_HOME%\bin\java.exe -Dfile.encoding=UTF-8 -jar "../tools\JSBuilder\JSBuilder2.jar" -v -p "./mobile-slx.jsb2" -d "../deploy"
-if %errorlevel% neq 0 exit /b %errorlevel% 
-xcopy ..\deploy\mobile-slx\*.* ..\applications\mobile\packages /Y
