c77bb97874de2de050e929cf183013ac59cc81e0
mmorton <mmorton@icanhascode.com>
2010-08-11 17:31:44 -0700

    initial work on demo application.

---
+++ demos/SDataClientApp/src/Application.js
@@ -0,0 +1,10 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Veryth
+ * Date: Aug 11, 2010
+ * Time: 12:34:28 AM
+ * To change this template use File | Settings | File Templates.
+ */
+(function(){
+            
+})();
