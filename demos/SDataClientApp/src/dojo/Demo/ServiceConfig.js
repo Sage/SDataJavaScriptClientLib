7ea56d87840018ecf58ed970b38ed58fdc192a64
mmorton <mmorton@icanhascode.com>
2010-08-17 01:00:34 -0700

    initial service config form layout

--- demos/SDataClientApp/src/dojo/Demo/ServiceConfig.js
+++ demos/SDataClientApp/src/dojo/Demo/ServiceConfig.js
@@ -1,13 +1,35 @@
 dojo.provide('Demo.ServiceConfig');
 dojo.require('Demo.View');
 dojo.require('dijit.layout.ContentPane');
+dojo.require('dojox.layout.TableContainer');
+dojo.require('dijit.form.TextBox');
+dojo.require('dijit.form.CheckBox');
+dojo.require('dijit.form.ComboBox');
+dojo.require('dijit.form.NumberSpinner');
 (function(){
     var T = new Simplate([
             '<div style="height: 100%; width: 100%">',
-                '<div dojoType="dijit.layout.BorderContainer" style="height: 100%; width: 100%" dojoAttachPoint="outerBC">',
-                    '<div dojoType="dijit.layout.ContentPane" region="top" splitter="true" style="height: 100px">top</div>',
-                    '<div dojoType="dijit.layout.ContentPane" region="center" splitter="true">center</div>',
-                    '<div dojoType="dijit.layout.ContentPane" region="bottom" splitter="true">bottom</div>',
+                '<div dojoType="dijit.layout.BorderContainer" style="height: 100%; width: 100%" dojoAttachPoint="container">',
+                    '<div dojoType="dijit.layout.ContentPane" region="center" splitter="true">',
+                        '<fieldset>',
+                        '<legend>Authentication</legend>',
+                        '<div dojoType="dojox.layout.TableContainer" cols="2" customClass="justLabels">',
+                            '<div dojoType="dijit.form.TextBox" title="User Name:" value=""></div>',
+                            '<div dojoType="dijit.form.TextBox" title="Password:" value=""></div>',
+                        '</div>',
+                        '</fieldset>',
+                        '<fieldset>',
+                        '<legend>URL Settings</legend>',
+                        '<div dojoType="dojox.layout.TableContainer" cols="2" customClass="justLabels">',
+                            '<div dojoType="dijit.form.ComboBox" title="Protocol:" value=""></div>',
+                            '<div dojoType="dijit.form.TextBox" title="Server:" value=""></div>',
+                            '<div dojoType="dijit.form.TextBox" title="Virtual Director:" value=""></div>',
+                            '<div dojoType="dijit.form.TextBox" title="Application:" value=""></div>',
+                            '<div dojoType="dijit.form.TextBox" title="Contract:" value=""></div>',
+                            '<div dojoType="dijit.form.TextBox" title="Data Set:" value=""></div>',
+                        '</div>',
+                        '</fieldset>',
+                    '</div>',
                 '</div>',
             '</div>'
         ]);
@@ -17,8 +39,8 @@ dojo.require('dijit.layout.ContentPane');
         postCreate: function()
         {
             this.inherited(arguments);
-            this.outerBC.startup();
-            this.outerBC.layout();
+            this.container.startup();
+            this.container.layout();
         }
     });
 })();
